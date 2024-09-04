8f9c9b432dd93d02dc30b55c61dc8555358c18b4
Lucas Kissmann <lucaskissmann@gmail.com>
2024-09-04 15:02:17 -0300

    initial commit

---
+++ public/categories.ts
@@ -0,0 +1,9 @@
+const categories = [
+	{ value: "FICÇÃO", label: "FICÇÃO" },
+	{ value: "ROMANCE", label: "ROMANCE" },
+	{ value: "TERROR", label: "TERROR" },
+	{ value: "CIÊNCIA", label: "CIÊNCIA" },
+	{ value: "INFANTIL", label: "INFANTIL" }
+];
+
+export default categories;
