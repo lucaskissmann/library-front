71d2d3d48fbe62497ba9e054ed12892f41d200da
Lucas Kissmann <lucaskissmann@gmail.com>
2024-09-13 08:53:38 -0300

    feat: add register authors page

---
+++ app/(routes)/admin/authors/register/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import FormAuthor from "./components/form-author";
+
+const RegisterAuthor = () => {
+	return (
+		<div className="pl-11 py-16">
+			<h1 className="text-4xl font-light">Cadastro da pessoa autora</h1>
+			<div className="flex mt-11 w-full">
+				<section className="w-1/3 pr-8">
+					<h2 className="text-2xl ">Criar Perfil</h2>
+					<h3 className="text-xl text-muted-foreground">Informações pessoais</h3>
+					<FormAuthor />
+				</section>
+				<section className="w-1/2 flex justify-center items-center">
+					<Image src={"/female-avatar-home.svg"} height={200} width={200} alt="Imagem de um personagem feminino"/>
+				</section>
+			</div>
+		</div>
+	);
+}
+
+export default RegisterAuthor;
