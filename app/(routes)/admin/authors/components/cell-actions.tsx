9a26debae81dcd3b14cd1345deb2318b7d2f5c75
Lucas Kissmann <lucaskissmann@gmail.com>
2024-09-17 10:50:33 -0300

    feat: removel author modal

--- app/(routes)/admin/authors/components/cell-actions.tsx
+++ app/(routes)/admin/authors/components/cell-actions.tsx
@@ -1,42 +1,72 @@
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription, DialogHeader } from "@/components/ui/dialog";
-import UpdateDialog from "@/components/update-dialog";
 import { useState } from "react";
 import { Author } from "@/types/Author";
+import axios from "axios";
+import { API_BASE_URL } from "@/config";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import AlertModal from "@/components/modals/alert-modal";
+import UpdateModal from "@/components/modals/update-modal";
 
 interface CellActionProps {
 	data: Author;
 }
 
 const CellAction: React.FC<CellActionProps> = ({ data }) => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+	const router = useRouter()
 
-	function handleOpenModal() {
-		setIsModalOpen(true);
-	}
+	const [isUpdateOpen, setIsUpdateOpen] = useState(false);
+	const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-	function handleCloseModal() {
-		setIsModalOpen(false);
-	}
+	const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`${API_BASE_URL}/authors/${data.id}`)
+      router.refresh()
+      toast.success('Autor removido com sucesso')
+    } catch (error) {
+      toast.error('Não foi possível remover o autor.')
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
 
 	return (
-		<div className="flex">
-			<Button 
-				variant="ghost"
-				className="text-[#2196F3] hover:text-[#21a6f3]"
-				onClick={handleOpenModal}
-			>
-				EDITAR
-			</Button>
-			<Button variant="ghost" className="text-[#2196F3] hover:text-[#21a6f3]">
-				EXCLUIR
-			</Button>
+		<>
+			<AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+        description={`O autor ${data.name} será removido`}
+      />
+			<UpdateModal
+        isOpen={isUpdateOpen}
+        onClose={() => setIsUpdateOpen(false)}
+				title={`Editar Autor`}
+        description={`Você está editando o autor ${data.name}`}
+				data={data}
+      />
+			<div className="flex">
+				<Button 
+					variant="ghost"
+					className="text-[#2196F3] hover:text-[#21a6f3]"
+					onClick={() => setIsUpdateOpen(true)}
+				>
+					EDITAR
+				</Button>
+				<Button 
+					variant="ghost"
+					className="text-[#2196F3] hover:text-[#21a6f3]"
+					onClick={() => setOpen(true)}
+				>
+					EXCLUIR
+				</Button>
+			</div>
+		</>
 
-			{isModalOpen && (
-				<UpdateDialog data={data} onClose={handleCloseModal} />
-			)}
-
-		</div>
 	);
 }
 
---
+++ components/modals/alert-modal.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { Button } from '../ui/button'
+import { Modal } from '../ui/modal'
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+  description: string;
+}
+const AlertModal: React.FC<AlertModalProps> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  loading,
+  description,
+}) => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
+
+  return (
+    <Modal
+      title="Você tem certeza?"
+      description={description}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+        <Button disabled={loading} variant="cancel" onClick={onClose}>
+          Cancelar
+        </Button>
+        <Button disabled={loading} variant="confirm" onClick={onConfirm}>
+          Excluir
+        </Button>
+      </div>
+    </Modal>
+  )
+}
+
+export default AlertModal
